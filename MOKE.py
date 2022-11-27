12a0e674ea3012e472d138a3ae19ac281f6fb16d
jacknondaniels <48773831+jacknondaniels@users.noreply.github.com>
2022-11-27 16:39:32 +0100

    Add files via upload

--- MOKE.py
+++ MOKE.py
@@ -4,9 +4,18 @@ from os import listdir, walk
 from os.path import isdir, join
 import sys
 import glob
-import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as lines
+from scipy.optimize import curve_fit
+
+def linear(x, a, b):
+    return a*x + b
+
+def arc(x, a, b, c):
+    return c*np.tanh(a*x) + b
+
+
 
 class single_mes:  # class for single measurement
     def __init__(self, name, path=None, phi=None, H=None):
@@ -20,23 +29,25 @@ class single_mes:  # class for single measurement
         phi = []
         H = []
         for line in f:
-            phi.append(float(line.split()[1]))
-            H.append(float(line.split()[0]))
+            h = float(line.split()[0])
+            if (h >= -250) and (h <= 250):
+                phi.append(float(line.split()[1]))
+                H.append(h)
         f.close()
-        self.phi = phi
-        self.H = H
+        self.phi = np.array(phi)
+        self.H = np.array(H)
 
-    def single_pic (self, name = None):
+    def single_pic (self, ax, name = None):
         if name == None:
             name = self.name
         exit_file = name + '.png'
-        fig, ax = plt.subplots(1, 1, figsize=(8, 8))
-        ax.set_ylabel("phi, milidegree")
-        ax.set_xlabel("H, Oe")
         ax.plot(self.H, self.phi, label=name)
-        ax.legend()
-        fig.savefig(exit_file)
-        plt.close()
+
+    def optimize (self, plt):
+        popt, pcov = curve_fit(arc, self.H, self.phi)
+        plt.plot (self.H, arc(self.H, *popt), label = 'fit: a=%5.3f, b=%5.3f, c=%5.3f' % tuple(popt))
+        plt.plot (self.H, self.phi-arc(self.H, *popt), label = 'minus')
+        return popt
 
 def read_dir (path):
     files = glob.glob(path + "/*.DAT")
@@ -52,12 +63,13 @@ def read_dir (path):
 
 mypath = "{}".format(sys.argv[1])
 mes = read_dir(mypath)
-exit_file = 'exit.png'
+exit_file = 'fitting.png'
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+for leaf in mes:
+    leaf.single_pic(ax)
+    leaf.optimize(ax)
 ax.set_ylabel("phi, milidegree")
 ax.set_xlabel("H, Oe")
-for leaf in mes:
-    ax.plot(leaf.H, leaf.phi, label=leaf.name)
 ax.legend()
 fig.savefig(exit_file)
 plt.close()
